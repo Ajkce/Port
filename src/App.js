6a3405042e7f19ef425bce330abaf7cb870c515d
Ajaya K C <kshettryajaya@gmail.com>
2022-05-31 23:36:33 +1000

    deploying

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Footer from "./Components/Footer/Footer";
 import Home from "./Components/Home/Home";
 import Navbar from "./Components/Navabr/Navbar";
 import Portfolio from "./Components/Portfolio/Portfolio";
-import Skills from "./Components/Skills/Skills";
+
 
 
 function App() {
@@ -22,7 +22,7 @@ function App() {
           
           <Home/>
           <About/>
-          <Skills/>
+        
           <Education/>
          
             
